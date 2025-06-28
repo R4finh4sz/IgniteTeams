95ab7054a7e55437cb03a715ee360b65b52a254e
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-06-28 19:36:16 -0300

    Update styles.tsx

--- src/Components/Loading/styles.tsx
+++ src/Components/Loading/styles.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components/native";
-import { theme } from "@theme/index";
 
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-`
+
+    background-color: ${({ theme }) => theme.colors.base.gray600};
+`;
