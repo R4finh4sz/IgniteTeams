2aa5107451deed9ef08bddd3c8fd54ddafb87576
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-06-28 01:29:13 -0300

    Feat: Added FontsLoaded and ActivityIndicator

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components"; 
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
@@ -6,9 +7,12 @@ import { theme } from "@theme/index";
 import { Groups } from "@screens/Groups";
 
 export default function app(){
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+
   return (
 <ThemeProvider theme={theme}>
-<Groups />
+{ fontsLoaded? <Groups /> : <ActivityIndicator/> }
 </ThemeProvider>
 
   )
