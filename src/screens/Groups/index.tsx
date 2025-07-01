a9c37ca56da301b455e687db1e3d68dbff069266
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-01 01:59:05 -0300

    Feat: Added Header container

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native';
 import {styles} from './styles'
 import { Container } from 'src/Components/Loading/styles';
+import { Header } from 'src/Components/Header';
 
 export function Groups() {
   return (
-    <Container></Container>
+    <Container>
+      <Header />
+    </Container>
   );
 }
