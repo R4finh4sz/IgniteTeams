c739efcb37e7a070de0231090d1e3694f03ad8b6
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-01 19:05:27 -0300

    Feat: Added Props and backbutton styles

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,19 @@
-import { Container, Logo, BackIcon } from "./styles";
+import { Container, Logo, BackIcon, BackButton } from "./styles";
 import logoImg from '@img/Logo.png';
 
+type Props = {
+    showBackButton?: boolean;
+}
 
-export function Header(){
+
+export function Header({showBackButton = false}: Props){
     return(
         <Container>
+            { showBackButton &&
+            <BackButton>
             <BackIcon />
+            </BackButton>
+}
             <Logo source={logoImg}/>
         </Container>
     )
