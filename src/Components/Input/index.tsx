71bd423e0a8f52bd8e8f97e55baf225685f4ffea
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 03:10:05 -0300

    Feat: Added const Colors and theme color in placeholder

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,8 +1,12 @@
 import { TextInputProps } from "react-native";
 import { Container } from './styles';
+import { useTheme } from "styled-components";
 
 export function Input({ ...rest }: TextInputProps) {
+    const { COLORS } = useTheme();
     return (
-        <Container {...rest} />
+        <Container 
+        placeholderTextColor={COLORS.GRAY_300}
+        {...rest} />
     );
 }
