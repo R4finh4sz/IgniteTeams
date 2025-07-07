1a899a15c904363603a8a2d04574928c01ec5366
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 01:11:32 -0300

    Fix: margins

--- src/Components/HighLight/styles.tsx
+++ src/Components/HighLight/styles.tsx
@@ -1,20 +1,23 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  width: 100% ;
-  margin: 32px 0;
+    width: 100%;
+    position: absolute;
+    top: 13.5%;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Title = styled.Text`
-    text-align: center;
-    font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
-    font-family: ${({ theme}) => theme.FONT_FAMILY.BOLD};
-    color: ${({ theme}) => theme.COLORS.WHITE};
+  text-align: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.WHITE};
 `;
 
 export const Subtitle = styled.Text`
-    text-align: center;
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-    font-family: ${({ theme}) => theme.FONT_FAMILY.REGULAR};
-    color: ${({ theme}) => theme.COLORS.GRAY_300};
+  text-align: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
 `;
