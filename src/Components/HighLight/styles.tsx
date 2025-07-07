541574e2c8016e0d4c75bd6c5825146ba4d8c877
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 01:46:46 -0300

    Fix: remove top and added margin

--- src/Components/HighLight/styles.tsx
+++ src/Components/HighLight/styles.tsx
@@ -1,15 +1,13 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-    width: 100%;
-    position: absolute;
-    top: 13.5%;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  margin: 32px 0;
 `;
 
 export const Title = styled.Text`
   text-align: center;
+
   font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   color: ${({ theme }) => theme.COLORS.WHITE};
@@ -17,6 +15,7 @@ export const Title = styled.Text`
 
 export const Subtitle = styled.Text`
   text-align: center;
+
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   color: ${({ theme }) => theme.COLORS.GRAY_300};
