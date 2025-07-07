2903220073821198e8d5028fcb0fca570f929385
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 01:45:41 -0300

    Fix: Container path

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
-import { Container } from 'src/Components/Loading/styles';
+import { Container } from './styles';
 import { Header } from 'src/Components/Header';
 import { HighLight } from 'src/Components/HighLight';
+import { GroupCard } from 'src/Components/GroupCard';
 
 export function Groups() {
   return (
@@ -9,6 +10,8 @@ export function Groups() {
       <HighLight 
       title='Turmas'
       subtitle='Jogue com a sua turma'/>
+
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
