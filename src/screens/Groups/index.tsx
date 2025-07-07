70d6f62a1c1c443cab91c6985dc0b57e4253f6b8
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 02:00:49 -0300

    Feat: Added a FlatList in Screen utilizing GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 import { Header } from 'src/Components/Header';
 import { HighLight } from 'src/Components/HighLight';
 import { GroupCard } from 'src/Components/GroupCard';
 
 export function Groups() {
-  const [groups, setgroups] = useState();
+  const [groups, setgroups] = useState<string[]>(['Galera de Cowboy', 'Amigos', 'Familia']);
   return (
     <Container>
       <Header  />
       <HighLight 
       title='Turmas'
       subtitle='Jogue com a sua turma'/>
-
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+        <GroupCard
+          title={item}
+        />
+      )}
+      />
     </Container>
   );
 }
