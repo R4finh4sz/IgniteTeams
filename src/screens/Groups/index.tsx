1c65d1236860be2e102a8ee299c104515d3216b7
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 01:53:03 -0300

    Feat: Added useState for the groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { Container } from './styles';
 import { Header } from 'src/Components/Header';
 import { HighLight } from 'src/Components/HighLight';
 import { GroupCard } from 'src/Components/GroupCard';
 
 export function Groups() {
+  const [groups, setgroups] = useState();
   return (
     <Container>
       <Header  />
