410863c58b4bbb8ada01400c55d5a4ebbd6d5f69
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-07 02:56:50 -0300

    Feat: Added new component input in screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "src/Components/Header";
 import { HighLight } from "src/Components/HighLight";
 import { Container, Content, Icon } from "./styles";
 import { Button } from "src/Components/Button";
+import { Input } from "src/Components/Input";
 
 export function NewGroup(){
     return (
@@ -13,8 +14,10 @@ export function NewGroup(){
             title='Nova turma'
             subtitle='Crie a turma para adicionar as pessoas'
             />
+            <Input/>
             <Button
             title='Criar'
+            style={{ marginTop: 20}}
             />
         </Content>
         </Container>
