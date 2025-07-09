4cfa35fb314e6255b9689e1a4cb23d3f71bf7eea
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-09 02:09:56 -0300

    Feat: Added FlatList, useState, Filter and styles

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,15 @@
 import { Header } from "src/Components/Header";
 import { HighLight } from "src/Components/HighLight";
-import { Container } from "./styles";
+import { Filter } from "src/Components/Filter";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import {ButtonIcon} from 'src/Components/ButtonIcon'
+import { Input } from "src/Components/Input";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players(){
+    const [team, setTeam] = useState('Time a');
+    const [players, setPlayers] = useState([]);
     return(
         <Container>
             <Header showBackButton />
@@ -12,7 +18,29 @@ export function Players(){
             title="Nome da turma"
             subtitle="Adicione a galera e separe os times"
             />
-            <ButtonIcon icon="home"/>
-        </Container>
+        <Form>
+            <Input 
+            placeholder="Nome da pessoa"
+            autoCorrect={false}
+            />
+            <ButtonIcon icon="add"/>
+        </Form>
+        <HeaderList>
+        <FlatList
+        data={['Time a', 'Time B']}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+        <Filter title={item} 
+        isActive={item === team}
+        onPress={() => setTeam(item)}
+        />
+        )}
+        horizontal
+        />
+        <NumberOfPlayers>
+        {players.length}
+        </NumberOfPlayers>
+        </HeaderList>
+                </Container>
     );
 }
