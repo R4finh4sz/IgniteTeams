a0a2a30e255ac83f6f9dbccfd0ceee6a6d4ae8ff
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-16 01:13:07 -0300

    Fix: font-size

--- src/Components/HighLight/styles.tsx
+++ src/Components/HighLight/styles.tsx
@@ -8,7 +8,7 @@ export const Container = styled.View`
 export const Title = styled.Text`
 text-align: center;
 ${({ theme }) => css`
-  font-size: ${theme.FONT_SIZE.LG}px; 
+  font-size: ${theme.FONT_SIZE.XL}px; 
     font-family: ${theme.FONT_FAMILY.BOLD };
     color: ${ theme.COLORS.WHITE };
 
