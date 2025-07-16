66ad43a434ad77f5956fbe5339f746ba7ae4f716
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-16 01:12:33 -0300

    Feat: addeed useState and useNavegation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,19 @@ import { HighLight } from "src/Components/HighLight";
 import { Container, Content, Icon } from "./styles";
 import { Button } from "src/Components/Button";
 import { Input } from "src/Components/Input";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
-export function NewGroup(){
+export function NewGroup() {
+
+
+    const [group, setgroup] = useState('');
+    const navigation = useNavigation();
+
+    function handleNew() {
+    navigation.navigate('players', { group })    
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -16,10 +27,12 @@ export function NewGroup(){
             />
             <Input
                 placeholder="teste"
+                onChangeText={setgroup}
             />
             <Button
             title='Criar'
             style={{ marginTop: 20}}
+            onPress={handleNew}
             />
         </Content>
         </Container>
