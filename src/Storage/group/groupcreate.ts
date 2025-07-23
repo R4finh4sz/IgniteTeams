350d9541b5d46dac222269eea3de232d0ec93ccf
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-07-23 00:35:06 -0300

    Feat: Added error handling

--- src/Storage/group/groupcreate.ts
+++ src/Storage/group/groupcreate.ts
@@ -1,11 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "../storageConfig";
-import { NewGroup } from "@screens/NewGroup";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "src/Utils/AppError";
 
 export async function groupCreate(newGroupName: string){
     try{
         const storedGroup = await groupsGetAll();
+
+        const groupAlreadyExists = storedGroup.includes(newGroupName);
+
+        if(groupAlreadyExists){
+            throw new AppError('Já existe um grupo cadastrado com esse nome');
+                }
 
         const storage = JSON.stringify([...storedGroup, newGroupName])
 
